48fcea53f1cf53e4125b1375eb03c700e764a93f
Joel <joelmounts@gmail.com>
2022-04-20 23:40:43 -0500

    testing auth error

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -11,18 +11,18 @@ auth = Blueprint('auth', __name__, template_folder='auth_templates')
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = UserLoginForm()
-    try:
-        if request.method == 'POST' and form.validate_on_submit():
-            email = form.email.data
-            password = form.password.data
-            user = User(email, password=password)
-            db.session.add(user)
-            db.session.commit()
-            flash(
-                f'You have successfully created a user account {email}', 'User-created')
-            return redirect(url_for('site.home'))
-    except:
-        raise Exception('Invalid form data: Please check your form')
+    # try:
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User(email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash(
+            f'You have successfully created a user account {email}', 'User-created')
+        return redirect(url_for('site.home'))
+    # except (error):
+        # raise Exception('Invalid form data: Please check your form')
     return render_template('sign_up.html', form=form)
 
 
